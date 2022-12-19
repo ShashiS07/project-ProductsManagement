6975b4da7d1d2279116a28b17dc3c8e709ff00fb
NATSU03 <vishwasw75@gmail.com>
2022-12-19 18:17:54 +0530

    1st day done

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -10,27 +10,28 @@ const passwordFormat = /^[a-zA-Z0-9@]{8,15}$/
 
 
 const createUser = async function (req, res){
-
-    try {
+   try {
         let data = req.body
-        console.log(data);
         let files = req.files
 
-        const {fname,lname,email,phone,password,address,profileImage} = data
+        let {fname,lname,email,phone,password,address} = data
 
+        if(typeof (address) == 'string') {address = JSON.parse(address)}
+      
+        // const jaddress = JSON.parse(address)
+        
         //--------------------------------requirements---------------------------------------------------
         if (Object.keys(data).length == 0) return res.status(400).send({ status: false, msg: "plz provide info for user" })
         if(!fname){return res.status(400).send({status:false, message:"Please provide title "})}
         if(!lname ){return res.status(400).send({status:false, message:"Please provide name "})}
         if(!phone ){return res.status(400).send({status:false, message:"Please provide phone "})}
         if(!password ){return res.status(400).send({status:false, message:"Please provide password "})}
-        if(!profileImage){return res.status(400).send({status:false, message:"plz provide profileImage"})}
+        if(!files){return res.status(400).send({status:false, message:"plz provide profileImage"})}
         if(!address){return res.status(400).send({status:false, message:"plz provide address"})}
-        if(!files[0]) {return res.status(400).send({ status: false, msg: "please provide image file" });}
 
         //----------------------------------validation---------------------------------------------------------
         if (!regexValidation.test(fname)) return res.status(400).send({ status: false, msg: "Please Enter Valid Name" })
-       if (!regexValidNumber.test(phone)) return res.status(400).send({ status: false, msg: "Please Enter Valid Phone Number" })
+        if (!regexValidNumber.test(phone)) return res.status(400).send({ status: false, msg: "Please Enter Valid Phone Number" })
         if (!emailValidator.validate(email)) return res.status(400).send({ status: false, msg: "Please Enter Valid email ID" })
         const validPassword = passwordFormat.test(password)
         if (!validPassword){return res.status(400).send({ status: false, msg: " Incorrect Password, It should be of 6-10 digits with atlest one special character, alphabet and number" });}
@@ -43,29 +44,58 @@ const createUser = async function (req, res){
         if (chkemail)return res.status(400).send({ status: false, msg: "email already exists" });
     
         //-------------------------------logic----------------------------------------------------------------------
-        const profileImageurl = await aws.uploadFile(files[0])
+        let profileImageurl = await aws.uploadFile(files[0])
         const salt = await bcrypt.genSalt(10);
         password = await bcrypt.hash(password, salt);
+        console.log(password);
         const data_to_create = {fname,lname,email,profileImageurl,phone,password,address}
+        
 
         const user = await userModel.create(data_to_create);
         return res.status(201).send({ status: true,msg:"Succes", data: user})
 
-
-     } catch (error) {res.status(500).send({ status: false, msg: error.message })}
+    } catch (error) {res.status(500).send({ status: false, msg: error.message })}
     }
 
 
+const login= async function(req,res){
+
+        try {
+        
+        const data= rq.body
+        if(!Object.keys(data).count==0||1){res.status(400).send({status:false,message:"plese provide email and password(they are manadatory)"})}
+        
+        let{email,password}=data
+        
+        const userData= await userModel.findOne({email:email,password:password,isDeleted:false})
+        if(!userData){
+            res.status(404).send({status:false,message:"please provide valid email and password"})
+        }
+        const token= jwt.sign({userId:userData._id},"ProductMnagementGroup24",{expiresIn: '12hr'})
+        
+        const tokenData={
+            userId:userData._id,
+            token:token
+        }
+        res.status(200).send({status:true,message: "User login successfull",data:tokenData})
+           
+        } catch (err) {
+          res.status(500).send({status:false,message:err.message})  
+        }
+        
+}
+        
 
 // ========================get api=======================================
 const getUser=async function(req,res){
   try{
     let userId=req.params.userId
+    console.log(userId);
     if(!userId) return res.status(400).send({status:false, message:"Please Provide UserID"})
   
     if(!isValid(userId)) return res.status({status:false, message:"Please Provide valid UserID"})
   
-    if(userId!==req.pass.userId) return res.status(401).send({status:false,message:"Authentication Failed"})
+    //if(userId!==req.pass.userId) return res.status(401).send({status:false,message:"Authentication Failed"})
    
     let userexist=await userModel.findOne({_id:userId})
     if(!userexist) return res.status(404).send({status:false, message:"User not found"})
@@ -90,4 +120,42 @@ const getUser=async function(req,res){
     return res.status(500).send({status:false,error:error.message})
   }
   }
-module.exports={createUser,getUser}
+
+//put api
+
+
+const update=async function(req,res){
+    try {
+        let data = req.params.userId
+        let update = req.body
+    
+        let { fname, lname,password ,phone,email } = update
+      
+        if (Object.keys(update).length == 0) { return res.status(400).send({ status: false, msg: "incomplete request data provide more data" }) }
+    
+        if (fname || lname || password || email) {
+          if (typeof (fname || lname || password || email) !== "string") {
+            return res.status(400).send({ status: false, msg: "be in string only" })
+          }
+        }
+        // if (tags || subcategory) {
+        //   if (typeof (tags || subcategory) !== "object") {
+        //     return res.status(400).send({ status: false, msg: "tags/subcategory should be in array of string only" })
+        //   }
+        // }
+        let checkisDleted = await --yModel.findOne({ _id: data, isDeleted: true })
+        
+        if (checkisDleted) return res.status(404).send({ status: false, msg: "no users found" })
+    
+    
+        let users = await --Model.findOneAndUpdate({ _id: data },
+          {
+            fname: fname, lname: lname, password: password, email:email
+            ,// $push: { tags: tags, subcategory: subcategory }
+          }, { new: true })
+        return res.status(200).send({ status: true, message:"successful" ,data:users })
+    } catch (err) { res.status(500).send({ status: false, msg: err.message }) }
+}
+    
+     
+module.exports={createUser,getUser,update,login}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router=express.Router()
-const {createUser} = require('../Controllers/userController')
+const {createUser,getUser,login,update} = require('../Controllers/userController')
 
 
 router.post('/register',createUser)
+router.post('/user/login',login)
+router.get('/user/:userId/profile',getUser)
+router.put('/user/:userId/profile',update)
+
 
 
 module.exports = router
