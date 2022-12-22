00470ea3c237be029edeea0f846fb17d39ee5ce1
ShashiS07 <shashiss0708@gmail.com>
2022-12-22 10:50:59 +0530

    phase 3 start

---
+++ src/Model/cartModel.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const cartSchema= new mongoose.Schema({
+    userId: {
+        type: ObjectId,
+        ref: 'user',
+        require: true,
+        unique: true
+    },
+    items: [{
+        productId: {
+            type: ObjectId,
+            ref: 'product',
+            require: true
+        },
+        quantity: {
+            type: Number,
+            require: true,
+            default: 1
+        }
+    }],
+    totalPrice: {
+        type: Number,
+        require: true
+    },
+    totalItems: {
+        type: Number,
+        require: true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('cart', cartSchema)
